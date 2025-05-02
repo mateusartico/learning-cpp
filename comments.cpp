0dfb7f9854ba0ebfd9ae2f0d9d9881369250148d
mateusartico <coelhomat5@gmail.com>
2025-05-02 11:42:51 -0300

    Using comments

---
+++ comments.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// This is a comment
+	/* The code below will print Hello, World!
+	to the screen, and it is amazing */
+	cout << "Hello, World!" << endl; // This is a comment
+	
+	return 0;
+}
