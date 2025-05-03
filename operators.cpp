6607d78feace0c8d8beb4d14125ec0e14096a3cd
mateusartico <coelhomat5@gmail.com>
2025-05-02 17:25:40 -0700

    Arithmetic operators

---
+++ operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Arithmetic
+	int x = 10;
+	int y = 9;
+	
+	int sum = x + y; // 19
+	int sub = x - y; // 1
+	int mult = x * y; // 90
+	int div = x / y; // 0
+	int mod = x % y; // 1
+	x++; // Increase 1 to x
+	y--; // Decrease 1 to y
+
+	return 0;
+}
