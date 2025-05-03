b1fa080670ed391ec57aa80fc96b90ea48e0a575
mateusartico <coelhomat5@gmail.com>
2025-05-03 15:18:09 -0700

    Adding other types of operators

--- operators.cpp
+++ operators.cpp
@@ -14,5 +14,36 @@ int main() {
 	x++; // Increase 1 to x
 	y--; // Decrease 1 to y
 
+	// Assignment
+	int a = 10; // Assignment operator =
+	a += 2; // Addition operator +
+	a -= 3; // Subtraction operator -
+	a *= 2; // Multiplication operator *
+	a /= 2; // Division operator /
+	a %= 5; // Modulo operator %
+	a &= 11; // Bitwise AND
+	a ^= 5;	// Bitwise XOR
+	a |= 3; // Bitwise OR
+	a <<= 2; // Left shift
+	a >>= 1; // Right shift
+	
+	// Comparison
+	int j = 10;
+	int i = 8;
+	cout << (j > i) << endl; // Greater than
+	cout << (j < i) << endl; // Less than
+	cout << (j == i) << endl; // Equal to
+	cout << (j >= i) << endl; // Greater than or equal to
+	cout << (j <= i) << endl; // Less than or equal to
+	cout << (j != i) << endl; // Not equal
+	
+	// Logical
+	int l = 4;
+	int m = 3;
+	int n = 5;
+	cout << (m > l && n == m) << endl; // Logical and
+	cout << (m < l || n == l) << endl; // Logical or
+	cout << !(m < l) << endl; // Logical not
+
 	return 0;
 }
