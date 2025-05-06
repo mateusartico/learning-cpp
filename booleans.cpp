03f92ad34f84a24aae21887ad527a8bb5a5b86cf
mateusartico <coelhomat5@gmail.com>
2025-05-06 18:36:57 -0300

    Adding more examples

--- booleans.cpp
+++ booleans.cpp
@@ -7,5 +7,16 @@ int main() {
 	cout << isCodingFun << endl; // Outputs 1 (true)
 	cout << isFishTasty << endl; // Outputs 0 (false)
 
+	int x = 10;
+	int y = 9;
+	cout << (x < y) << endl; // Outputs 0 (false)
+	cout << (10 > 9) << endl; // Outputs 1 (true)
+	cout << (x == 10) << endl; // Outputs 1 (true)
+
+	// Real-life example
+	int myAge = 25;
+	int votingAge = 18;
+	cout << (myAge >= votingAge) << endl;
+
 	return 0;
 }
