ec1011a982c8f071dbe86b5f90be463ef45f27f7
mateusartico <coelhomat5@gmail.com>
2025-05-08 18:03:04 -0300

    Improving code readability by adding comments and properly closing lines

--- conditionals.cpp
+++ conditionals.cpp
@@ -2,33 +2,35 @@
 using namespace std;
 
 int main(void) {
+    // Simple if statement: checks if 20 is greater than 18
     if (20 > 18) {
-        cout << "20 is greater than 18";
+        cout << "20 is greater than 18" << endl;
     }
 
+    // Comparing two variables using if
     int x = 20;
     int y = 18;
     if (x > y) {
-    cout << "x is greater than y";
+        cout << "x is greater than y" << endl;
     }
 
+    // if-else statement: outputs different messages based on the time
     int time = 20;
     if (time < 18) {
-    cout << "Good day.";
+        cout << "Good day." << endl;
     } else {
-    cout << "Good evening.";
+        cout << "Good evening." << endl;
     }
 
-    int time = 22;
+    // if-else if-else chain: multiple conditions to check time ranges
+    time = 22;
     if (time < 10) {
-    cout << "Good morning.";
+        cout << "Good morning." << endl;
     } else if (time < 20) {
-    cout << "Good day.";
+        cout << "Good day." << endl;
     } else {
-    cout << "Good evening.";
+        cout << "Good evening." << endl;
     }
-
-
 
     return 0;
 }
